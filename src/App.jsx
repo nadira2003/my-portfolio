e67c1f0c2d8627fe91e2de92c13045f4a936c4a7
nadira2003 <80900769+nadira2003@users.noreply.github.com>
2025-05-07 02:58:57 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
+
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
-    <BrowserRouter>
+  
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
@@ -20,7 +20,7 @@ const App = () => {
           <StarsCanvas />
         </div>
       </div>
-    </BrowserRouter>
+  
   );
 }
 
